b7f67eeb1adf369faf4a892396cc1e5cfd2adf49
alexdnf <152063278+alexdnf@users.noreply.github.com>
2024-10-22 21:04:17 +0300

    Update Task22.java

--- Task22.java
+++ Task22.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Task22 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первую строку");
-        String a = scanner.nextLine();
-        System.out.println("Введите вторую стоку");
-        String b = scanner.nextLine();
+        
+        String a = "Hello"
+        
+        String b = "Hi"
 
         if (a.equals(b)) {
             System.out.println("Строки идентичны");
